ca41cf2c93ea8f2c88c2f73455b5d66f894d62de
Lukas Krecan <lukas@krecan.net>
2015-12-06 20:59:10 +0100

    AST parsing works

---
+++ project.clj
@@ -0,0 +1,5 @@
+(defproject cloflle "1.0.0-SNAPSHOT"
+  :description "Clojure in Truffle"
+  :source-paths ["src/main/clojure"]
+  :test-paths ["src/test/clojure"]
+  :dependencies [[org.clojure/clojure "1.7.0"], [org.clojure/tools.analyzer.jvm "0.6.9"]])
